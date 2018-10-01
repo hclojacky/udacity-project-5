95de4b38a431622457019927821e9473e05b6cb0
Jacky Lo <jackylo.hc@gmail.com>
2018-10-01 16:24:46 +0800

    update: service worker js path

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -186,7 +186,7 @@ sw = () => {
    if (!navigator.serviceWorker) return;
 
    console.log('Service Worker: Supported');
-   navigator.serviceWorker.register('../sw.js').then(function(reg){
+   navigator.serviceWorker.register('/sw.js').then(function(reg){
       console.log('Service Worker: Registered')
    }).catch(function(error) {
       console.log(`Service Worker: Error ${error}`);
